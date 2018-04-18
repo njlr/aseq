1d4f81de304f14dd582ac554b38eea905f685f46
njlr <njlr@users.noreply.github.com>
2018-04-18 22:15:06 +0100

    * Adds async support to map

--- src/map.js
+++ src/map.js
@@ -1,7 +1,10 @@
 import { fromGenerator } from './from-generator.js';
 
-export const map = f => xs => fromGenerator(async function * () {
-  for await (const x of xs) {
-    yield f(x);
-  }
-});
+export const map = f => {
+  const fAsync = async x => f(x);
+  return xs => fromGenerator(async function * () {
+    for await (const x of xs) {
+      yield fAsync(x);
+    }
+  });
+};
--- src/map.test.js
+++ src/map.test.js
@@ -15,3 +15,11 @@ it('map should work for a simple case', async () => {
     |> aseq.toArray);
   expect(xs).toEqual([ 2, 4, 6 ]);
 });
+
+it('map should work for an async simple case', async () => {
+  const xs = await ([ 1, 2, 3 ] 
+    |> aseq.fromIterable 
+    |> aseq.map(async x => x * 2) 
+    |> aseq.toArray);
+  expect(xs).toEqual([ 2, 4, 6 ]);
+});
