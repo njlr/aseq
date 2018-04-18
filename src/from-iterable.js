27d8802700cdbfffe2342483ebc3bc5d0f9bbb85
njlr <njlr@users.noreply.github.com>
2018-04-18 22:16:04 +0100

    * Fixes export of fromIterable

--- src/from-iterable.js
+++ src/from-iterable.js
@@ -1,5 +1,5 @@
 import { fromGenerator } from './from-generator.js'
 
-export const fromIterable = xs => fromGenerator(async function * () {
+export const fromGenerator = xs => fromGenerator(async function * () {
   yield * xs;
 });
